150aaab641a6a013d021c527c86a1dabcf3cd435
Filipe Rodrigues da Silva <lipe_rsilva@hotmail.com>
2024-08-20 19:59:45 -0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Filipe Rodrigues da Silva on 20/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
