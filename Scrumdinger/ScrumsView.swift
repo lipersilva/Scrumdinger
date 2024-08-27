88b9d63287501b3c633c1fa7d45642d9754c9ab6
Filipe Rodrigues da Silva <lipe_rsilva@hotmail.com>
2024-08-26 21:54:10 -0300

    Navigate between screens

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ScrumsView: View {
 	let scrums: [DailyScrum]
 	
-    var body: some View {
+	var body: some View {
 		NavigationStack {
 			List(scrums) { scrum in
 				NavigationLink(destination: DetailView(scrum: scrum)) {
@@ -18,15 +18,15 @@ struct ScrumsView: View {
 				}
 				.listRowBackground(scrum.theme.mainColor)
 			}
-		}
-		.navigationTitle("Daily Scrums")
-		.toolbar {
-			Button(action: {}) {
-				Image(systemName: "plus")
+			.navigationTitle("Daily Scrums")
+			.toolbar {
+				Button(action: {}) {
+					Image(systemName: "plus")
+				}
+				.accessibilityLabel("New Scrum")
 			}
-			.accessibilityLabel("New Scrum")
 		}
-    }
+	}
 }
 
 struct ScrumsView_Previews: PreviewProvider {
